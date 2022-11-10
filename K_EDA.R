b47715bd0a8b2524c1f430c270a0c1452bde3131
Kristina Bratkova <kbratkov0@gmail.com>
2022-11-10 12:42:34 +0000

    Load and map climate_action_data.geojson (local)

--- K_EDA.R
+++ K_EDA.R
@@ -1,8 +1,10 @@
+library(tmap)
 # read excel file
 CO2emis = readxl::read_excel("data/CO2emissionsbycountry.xlsx",
                               skip = 2)
 
 # check if read correctly
+# NOTE: uses 3-letter country code
 CO2emis[1:5,1:10]
 
 # count NA values in each column
@@ -15,4 +17,18 @@ names(CO2emis[,colSums(is.na(CO2emis))==266])
 names(CO2emis[,colSums(is.na(CO2emis))!=266 & colSums(is.na(CO2emis)) > 30])
 # suggests 1992-2019 is best choice
 
+# load country shapefile
+# NOTE: uses iso_a2 (2-letter country code)
+world = spData::world
 
+wws <- read.csv("data/wvs.csv")
+# NOTE: world view sirvey uses 3-letter country codes
+
+# load geojson file
+# Let's read the jeoJson file that is stored on the web with the geojsonio library:
+library(geojsonio)
+library(sf)
+# spdf <- geojson_read("https://raw.githubusercontent.com/GretaTimaite/UNBigDataHackathon2022/main/climate_action_data.geojson",  what = "sp")
+# load locally for now
+sfdf <- geojson_read("data/climate_action_data.geojson",what="sp") %>% st_as_sf()
+sfdf %>% tm_shape() + tm_polygons(col="ren_2018")

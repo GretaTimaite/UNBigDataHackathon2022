e50efc7b68d89745ec941473f355510e8e6f8453
Kristina Bratkova <kbratkov0@gmail.com>
2022-11-10 22:21:34 +0000

    Include static plot study to be interactive in the dashboard

--- K_EDA.R
+++ K_EDA.R
@@ -39,13 +39,17 @@ sfdf %>% tm_shape() + tm_polygons(col="ren_2018")
 
 # analysis to make Shiny plots of EDA per country
 #coln <- sfdf %>% st_drop_geometry() %>% filter(name_long=="Kenya") %>% select(starts_with("dis")) %>% 
-  colnames()
-coln <- sfdf %>% filter(name_long=="Kenya") st_drop_geometry() %>% select(-c(dis_Country,dis_ISO3,co2_Country.Name,gdpPercap)) %>%
+ # colnames()
+coln <- sfdf %>% filter(name_long=="Kenya") %>%  st_drop_geometry() %>% select(-c(dis_Country,dis_ISO3,co2_Country.Name,gdpPercap)) %>%
   select(starts_with(c("gdp"))) %>% names()
 coln1 <- as.numeric(substr(coln,nchar(coln)-4+1,nchar(coln)))
 values <- sfdf %>% st_drop_geometry() %>%filter(name_long=="Kenya") %>%  select(-c(dis_Country,dis_ISO3,co2_Country.Name,gdpPercap)) %>%
-  select(starts_with(c("gdp"))) %>% as.vector()
+  select(starts_with(c("gdp"))) %>% unname() %>%  as_vector() 
 plot(x=coln1,y=values)
 # library(stringr)
+df <- data.frame(coln1,values)
+ggplot(df) + geom_line(aes(x=coln1,y=values))  + geom_point(aes(x=coln1,y=values),lwd=2) +
+  theme_minimal() 
+
 # str_sub(coln, end=-4)
 #substr(A, nchar(A)-3+1, nchar(A))

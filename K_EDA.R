98a5a6b05e229cfe0c3c82291928247fb1e77b01
Kristina Bratkova <kbratkov0@gmail.com>
2022-11-11 01:10:53 +0000

    Add WVS env opinion plot to be made interactive in dashboard

--- K_EDA.R
+++ K_EDA.R
@@ -68,3 +68,46 @@ OSM_sf <- world %>% select(name_long) %>%  left_join(OSM1,by=c("name_long"="Coun
 tm_shape(OSM_sf) +
   tm_polygons(col= "2022",palette=viridis(n=7),alpha = 0.5)
 
+# incorporate data from Eric
+wvs %>% names()
+sfdf %>% st_drop_geometry() %>% select(name_long,dis_ISO3)
+wv <- wvs %>% select(country_5) %>% left_join((sfdf %>% st_drop_geometry() %>% select(name_long,dis_ISO3)),by=(c("country_5"="name_long")))
+theme_set(theme_light())
+# Explore how responses have changed over time
+
+env_count <- wvs %>% 
+  # Select evn columns
+  mutate("country_5"=as_vector( wv[,2])) %>% 
+  filter(country_5=="ARG" |country_4=="ARG" |country_6=="ARG" |country_7=="ARG"  ) %>% 
+  select(contains("env")) %>% 
+  #filter(if_any(everything(), is.na)) %>% 
+  # Reshape data for easy analysis
+  pivot_longer(everything(), names_to = "env", values_to = "opinion") %>% 
+  #mutate(across(everything()))
+  # Drop missing values
+  drop_na() %>% 
+  mutate(opinion = factor(opinion)) %>% 
+  # Count number of respondents in each category
+  count(env, opinion) %>% 
+  group_by(env) %>% 
+  mutate(total = sum(n)) %>% 
+  ungroup() %>% 
+  mutate(pct = n/total) %>% 
+  # Rename rows
+  mutate(env = case_when(
+    env == "env_4_num" ~ "wave_4",
+    env == "env_5_num" ~ "wave_5",
+    env == "env_6_num" ~ "wave_6",
+    env == "env_7_num" ~ "wave_7"
+  ))
+
+# Visualize this
+env_count %>% 
+  ggplot(mapping = aes(x = env, y = pct*100)) +
+  geom_col(aes(fill = opinion), position = "dodge", alpha = 0.8) +
+  paletteer::scale_fill_paletteer_d("ggthemes::Tableau_10",
+                                    labels=c("protect environment", " Economic growth", "Other")) +
+  ggtitle("Protecting environment vs Economic growth") +
+  labs(x = "survey period",
+       y = "% of respondents in survey") +
+  theme(plot.title = element_text(hjust = 0.5))
